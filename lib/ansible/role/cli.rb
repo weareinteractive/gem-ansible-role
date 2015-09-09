ca210b588f2cef62e5d1cb9b2728e0efd351fba1
franklin <franklin@weareinteractive.com>
2015-09-09 10:33:40 +0200

    added docgen

--- lib/ansible/role/cli.rb
+++ lib/ansible/role/cli.rb
@@ -1,5 +1,8 @@
+require "erb"
 require "git"
 require "thor"
+require "yaml"
+require "ostruct"
 
 module Ansible
   module Role
@@ -11,40 +14,79 @@ module Ansible
       # Tasks
       # -----------------------------------------------------------------------
 
-      desc "init PATH", "Initialize new role"
+      desc "init", "Initialize new role"
       def init()
 
         @data = {}
 
-        say "Role info", :yellow
-        @data[:name] = self.class.prompt "Name", nil, true
-        @data[:author] = self.class.prompt "Author", ENV['USER'], true
-        @data[:company] = self.class.prompt "Company", nil, true
-        @data[:description] = self.class.prompt "Description", nil, true
-        @data[:min_ansible_version] = self.class.prompt "Min ansible version", "1.9", true
-        @data[:category] = self.class.prompt "Category (system, web, ...)", nil, true
+        # check empty directory
+        abort "Role directory (#{Dir.getwd}) is not empty!" unless Dir["#{Dir.getwd}/*"].empty?
+
+        # initializing git repo
+        g = Git.init(Dir.getwd)
+
+        say "To generate your role, please give me some info about:\n\n", :yellow
+
+        say "• Author", :yellow
+        @data[:author] = prompt "  Name", "#{g.config('user.name')} <#{g.config('user.email')}>", true
+        @data[:company] = prompt "  Company", nil, true
+        puts ""
+
+        say "• Role", :yellow
+        @data[:name] = prompt "  Name", nil, true
+        @data[:description] = prompt "  Description", nil, true
+        @data[:category] = prompt "  Category (system, web, ...)", nil, true
+        @data[:min_ansible_version] = prompt "  Min ansible version", "1.9", true
         @data[:var_name] = @data[:name].sub("-", "_")
+        puts ""
 
-        say "GitHub info", :yellow
-        @data[:github_user] = self.class.prompt "GitHub user", @data[:author], true
-        @data[:github_name] = self.class.prompt "GitHub repository name", File.basename(Dir.getwd)
+        say "• GitHub", :yellow
+        @data[:github_user] = prompt "  User", g.config('user.name'), true
+        @data[:github_name] = prompt "  Repository", File.basename(Dir.getwd)
         @data[:github_path] = "#{@data[:github_user]}/#{@data[:github_name]}"
+        puts ""
 
-        say "Galaxy info", :yellow
-        @data[:galaxy_id] = self.class.prompt "Galaxy role ID", 0
-        @data[:galaxy_user] = self.class.prompt "Galaxy user", @data[:github_user]
+        say "• Ansible Galaxy", :yellow
+        @data[:galaxy_id] = prompt "  Role ID", 0
+        @data[:galaxy_user] = prompt "  User", ENV['username']
+        puts ""
 
         @data[:role_name] = "#{@data[:galaxy_user]}.#{@data[:name]}"
 
-        print @data.inspect
-
+        # render templates
         directory  "templates/ansible/role/init", Dir.getwd
+
+        # generate docs
+        docgen
+
+        # add and commit files
+        g.add()
+        g.commit('initial commit')
       end
 
-      # -----------------------------------------------------------------------
-      # Public methods
-      # -----------------------------------------------------------------------
+      desc "docgen", "Generate README file"
+      def docgen
+        @data = {}
+
+        @data[:defaults] = get_yaml("defaults")
+        @data[:meta] = get_yaml("meta")
+        @data[:tests] = get_yaml("tests")
+        @data[:vars] = get_yaml("vars")
+
+        @data[:files] = {}
+        @data[:files][:defaults] = get_yaml_files("defaults")
+        @data[:files][:tests] = get_yaml_files("tests")
+        @data[:files][:vars] = get_yaml_files("vars")
+
+        # render template
+        template  "templates/ansible/role/docgen/README.md.tt", "README.md"
+      end
+
       no_tasks do
+
+        # ---------------------------------------------------------------------
+        # Public methods
+        # ---------------------------------------------------------------------
 
         def dot()
           "."
@@ -54,27 +96,52 @@ module Ansible
           @data[:name]
         end
 
-      end
+        # ---------------------------------------------------------------------
+        # Private methods
+        # ---------------------------------------------------------------------
 
-      # -----------------------------------------------------------------------
-      # Public static methods
-      # -----------------------------------------------------------------------
+        private
 
-      def self.prompt(msg, default = nil, required = false)
-        loop do
-          msg += " [#{default}]" if default != nil
-          print "#{msg}: "
-          ret = $stdin.gets.chomp
-          if ret != ""
-            return ret
-          elsif ret == "" && default != nil
-            return default
-          elsif ret == "" && !required
-            return ret
+        def abort(msg)
+           say msg, :red
+           exit(1)
+        end
+
+        def prompt(msg, default = nil, required = false)
+          loop do
+            msg += " [#{default}]" if default != nil
+            print "#{msg}: "
+            ret = $stdin.gets.chomp
+            if ret != ""
+              return ret
+            elsif ret == "" && default != nil
+              return default
+            elsif ret == "" && !required
+              return ret
+            end
           end
         end
-      end
 
+        def get_yaml(path)
+          data = {}
+          Dir["#{Dir.getwd}/#{path}/*.yml"].each do |file_name|
+            basename = File.basename(file_name, '.yml')
+            data.store(:"#{basename}", YAML::load_file(file_name))
+          end
+          data
+        end
+
+        def get_yaml_files(path)
+          data = {}
+          Dir["#{Dir.getwd}/#{path}/*.yml"].each do |file_name|
+            basename = File.basename(file_name, '.yml')
+            file = File.open(file_name, "rb")
+            data.store(:"#{basename}", file.read)
+          end
+          data
+        end
+
+      end
     end
   end
 end
